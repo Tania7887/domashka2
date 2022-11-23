f276db549e181eb8493b6d8b4a4d728becd3f63f
Sidorova Tania <sidorovatania3@gmail.com>
2022-11-23 20:39:21 +0300

    Добавила 13 задачу

---
+++ dom13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.Write("Enter the number: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+
+
+if (numberText.Length > 2){
+  Console.WriteLine("Third digit -> " + numberText[2]);
+}
+else {
+  Console.WriteLine("->  No third digit");
+}
