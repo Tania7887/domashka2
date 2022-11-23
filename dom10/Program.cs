7beaa4130e5e63100eb14bc2d892b4015ad08485
Sidorova Tania <sidorovatania3@gmail.com>
2022-11-23 20:23:42 +0300

    Добавила 10 задачу

---
+++ dom10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Enter a threeDigitNumber: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+
+
+Console.WriteLine("The second digit of this number -> " + stringNumber[1]);
+
+
+
+
+
+
+
+
+
+
